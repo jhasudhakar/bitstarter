6dc2a05e0d2b738e17e327d07c2bf96388610b53
user <user@ubu12.(none)>
2013-07-10 08:53:12 +0530

    web.js reading from index.html

--- web.js
+++ web.js
@@ -3,7 +3,14 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+
+
+  var fs = require('fs');
+  var infile = "index.html"
+  var inText = new Buffer(64);
+  inText = fs.readFileSync(infile);
+
+  response.send(inText.toString());
 });
 
 var port = process.env.PORT || 5000;
